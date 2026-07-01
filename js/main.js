1856ad9791ee7eb1ae19059f6501732c75e156f9
sohee0521 <qkrthgml0417@gmail.com>
2026-07-02 04:41:50 +0900

    test5

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ const con2Info = [
     location_2: "경남도미술관",
   },
   {
-    img: "../img/con2Img2.png",
+    img: "./img/con2Img2.png",
     tag: "디자인",
     title: "베르디(VERDY)개인전 《아이 빌리브 인 미(I Believe in Me)》",
     date: "2026.05.22  ~ 2026.07.19",
@@ -16,7 +16,7 @@ const con2Info = [
     location_2: "마르코미술관",
   },
   {
-    img: "../img/con2Img3.png",
+    img: "./img/con2Img3.png",
     tag: "회화",
     title: "정보경 개인전 《사유를 앞질러 Outruns Thought》",
     date: "2026.06.12 ~ 2026.07.05",
@@ -24,7 +24,7 @@ const con2Info = [
     location_2: "OMG SEOUL",
   },
   {
-    img: "../img/con2Img4.png",
+    img: "./img/con2Img4.png",
     tag: "회화",
     title: "추사의 그림 수업",
     date: "2026.04.07 ~ 2026.07.05 ",
@@ -53,19 +53,19 @@ for (let i = 0; i < con2Info.length; i++) {
 
 const con3Info = [
   {
-    img1: "../img/con3Img1_1.png",
-    img2: "../img/con3Img1_2.png",
-    img3: "../img/con3Img1_3.png",
+    img1: "./img/con3Img1_1.png",
+    img2: "./img/con3Img1_2.png",
+    img3: "./img/con3Img1_3.png",
   },
   {
-    img1: "../img/con3Img2_1.png",
-    img2: "../img/con3Img2_2.png",
-    img3: "../img/con3Img2_3.png",
+    img1: "./img/con3Img2_1.png",
+    img2: "./img/con3Img2_2.png",
+    img3: "./img/con3Img2_3.png",
   },
   {
-    img1: "../img/con3Img3_1.png",
-    img2: "../img/con3Img3_2.png",
-    img3: "../img/con3Img3_3.png",
+    img1: "./img/con3Img3_1.png",
+    img2: "./img/con3Img3_2.png",
+    img3: "./img/con3Img3_3.png",
   },
 ];
 
@@ -82,47 +82,47 @@ for (let i = 0; i < con3Info.length; i++) {
 
 const con5Info = [
   {
-    img: "../img/con5Img1.png",
+    img: "./img/con5Img1.png",
     title:
       "경계의 시간 ‘새벽’, 불완전한 존재들이 건네는 조용한 위로: 《새벽에 남은 새》 ",
   },
   {
-    img: "../img/con5Img2.png",
+    img: "./img/con5Img2.png",
     title: "국내 최대 규모, 우고 론디노네 개인전 《BURN TO SHINE》",
   },
   {
-    img: "../img/con5Img3.png",
+    img: "./img/con5Img3.png",
     title:
       "<예화랑/ 베니스비엔날레 한국관 건립 30주년 기획전 《30 Years: Passages》",
   },
   {
-    img: "../img/con5Img4.png",
+    img: "./img/con5Img4.png",
     title: "신미정 애도와 복원의 미술: 망각의 이미지와 은폐된 이름으로부터",
   },
   {
-    img: "../img/con5Img5.png",
+    img: "./img/con5Img5.png",
     title: "인공지능을 활용하는 창작자에게 묻는다: 동시대 예술가 36인 인터뷰",
   },
   {
-    img: "../img/con5Img1.png",
+    img: "./img/con5Img1.png",
     title:
       "경계의 시간 ‘새벽’, 불완전한 존재들이 건네는 조용한 위로: 《새벽에 남은 새》 ",
   },
   {
-    img: "../img/con5Img2.png",
+    img: "./img/con5Img2.png",
     title: "국내 최대 규모, 우고 론디노네 개인전 《BURN TO SHINE》",
   },
   {
-    img: "../img/con5Img3.png",
+    img: "./img/con5Img3.png",
     title:
       "<예화랑/ 베니스비엔날레 한국관 건립 30주년 기획전 《30 Years: Passages》",
   },
   {
-    img: "../img/con5Img4.png",
+    img: "./img/con5Img4.png",
     title: "신미정 애도와 복원의 미술: 망각의 이미지와 은폐된 이름으로부터",
   },
   {
-    img: "../img/con5Img5.png",
+    img: "./img/con5Img5.png",
     title: "인공지능을 활용하는 창작자에게 묻는다: 동시대 예술가 36인 인터뷰",
   },
 ];
